a678a51850fc1c10dcc6c50eca5994e0cc1711e2
Malakai <poncemalakai@gmail.com>
2025-11-14 22:59:49 -0800

    building

--- Lab_32.cpp
+++ Lab_32.cpp
@@ -2,12 +2,22 @@
 #include <cstdlib>
 #include <ctime>
 #include <deque>
+using namespace std;
 
 const int initalSize = 2;
 
 int main() {
     srand(time(0));
+
+    deque<Car> carDeque;
+
+    for (int i = 0; i < initalSize; i++) {
+        Car c;
+        carDeque.push_back(c);
+    }
+
+    cout << "Start queue" << endl;
+    
+
 }
 
-
-// Git test 
